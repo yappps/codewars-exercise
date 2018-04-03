bd1eb9475ba9f367a0b77a2058390add1b011ee7
davidtan <davidtan@thoughtworks.com>
2018-04-03 09:44:59 +0800

    Add second kata

---
+++ test/sortDigitsInDescendingOrder.test.js
@@ -0,0 +1,4 @@
+// Test cases: actual --> expected
+// sortDigitsInDescendingOrder(21445) --> 54421
+// sortDigitsInDescendingOrder(145263) --> 654321
+// sortDigitsInDescendingOrder(1254859723) --> 9875543221
