821dec13c7b1c64d8ebe08f07ff62b681f5035b2
davidtan <davidtan@thoughtworks.com>
2018-04-04 10:34:22 +0800

    Add third kata

---
+++ src/decodeMessage.js
@@ -0,0 +1,11 @@
+/* Write a function to decode a given input string.
+Each letter is paired with the letter that it coincides with when the alphabet is reversed.
+For example: "a" is encoded with "z", "b" with "y", "c" with "x", etc
+Spaces are not encoded/decoded. For example: "gsrh nvhhztv" will be decoded as "this message"
+*/
+
+const decodeMessage = (string) => {
+
+};
+
+module.exports = decodeMessage;
---
+++ test/decodeMessage.test.js
@@ -0,0 +1,11 @@
+// Test cases: actual --> expected
+// decodeMessage("a") --> "z"
+// decodeMessage("z") --> "a"
+// decodeMessage("b") --> "y"
+// decodeMessage("y") --> "b"
+// decodeMessage("ab") --> "zy"
+// decodeMessage("sr") --> "hi"
+// decodeMessage("svool") --> "hello"
+
+// decodeMessage("gsrh nvhhztv") --> "this message"
+// decodeMessage("r slkv mlylwb wvxlwvh gsrh nvhhztv") --> "i hope nobody decodes this message"
