294694fec1f34ede3527ee289a2e6b8a5967e184
davidtan <davidtan@thoughtworks.com>
2018-04-09 09:26:34 +0800

    Add new kata - findDigit

---
+++ test/findDigit.test.js
@@ -0,0 +1,7 @@
+// findDigit(5673, 4)     returns 5
+// findDigit(129, 2)      returns 2
+// findDigit(-2825, 3)    returns 8
+// findDigit(-456, 4)     returns 0
+// findDigit(0, 20)       returns 0
+// findDigit(65, 0)       returns -1
+// findDigit(24, -8)      returns -1
