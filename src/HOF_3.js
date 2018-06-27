46ee9056d59702dc02a9f5df5728cbe99b91931e
yappps <yapyuanxin@gmail.com>
2018-06-28 00:40:28 +0800

    added another method

--- src/HOF_3.js
+++ src/HOF_3.js
@@ -1,4 +1,14 @@
-const isRubyComing = list =>
-  list.filter(dev => dev.language === "Ruby").length === 0 ? false : true;
+// My ans
+// const isRubyComing = list =>
+//   list.filter(dev => dev.language === "Ruby").length === 0 ? false : true;
+
+
+// A clever one : Using `some` instead of `filter`
+/* 
+- The `some()` method tests whether at least one element in the array passes the test implemented by the provided function.
+- Return value : true if the callback function returns a truthy value for any array element; otherwise, false.
+
+*/
+const isRubyComing = list => list.some(dev => dev.language === "Ruby");
 
 module.exports = isRubyComing;
