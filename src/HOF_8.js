021e168a7048d02e100142cdd2e14973c94c9806
yappps <yapyuanxin@gmail.com>
2018-12-29 12:11:05 +0800

    Add comment

--- src/HOF_8.js
+++ src/HOF_8.js
@@ -2,7 +2,7 @@ const continents = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
 const allContinents = list => {
   let data = {};
   list.forEach(dev => {
-      /* 
+    /*
       Syntax: objName.keyName = (objName.keyName || defaultValue)
       */
     data[dev.continent] = (data[dev.continent] || 0) + 1;
@@ -10,4 +10,9 @@ const allContinents = list => {
   return Object.keys(data).length === continents.length ? true : false;
 };
 
+/* 
+A better approach using `every` and `some`
+const allContinents = list =>
+continents.every(continent => list.some(dev => continent == dev.continent));
+*/
 module.exports = allContinents;
